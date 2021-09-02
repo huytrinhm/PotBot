8dd3d403a23022459a66d25dbe068979f6019fbb
Huy Trịnh <huytrinhm@gmail.com>
2021-09-02 18:15:56 +0700

    Starter code

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Bot is ready');
+});
+
+client.login(process.env.BOT_TOKEN);
