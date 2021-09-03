9c9fbdc2b8a5a8fd88e84645bb897717af5c261c
Huy Trịnh <huytrinhm@gmail.com>
2021-09-03 21:54:49 +0700

    Add basic quiz function

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ const cmds = [
 	'freeze',
 	'unfreeze',
 	'info',
-	'help'
+	'help',
+	'quiz',
+	'random-quiz',
+	'endless-quiz',
+	'add-quiz'
 ];
 const opts = [
 	'name',
@@ -110,6 +114,20 @@ async function setCommand(changeId, msg, opt) {
 		}).catch(() => {
 
 		});
+	}
+}
+
+function sendQuiz(msg, data, id) {
+	if(data.type == 0) {
+		var em = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle(`${data.score} points question`)
+			.setDescription(data.question)
+			.addField('Answers', `||${data.answers.join('|')}||`)
+			.addField('Tags', data.tag.join(', '))
+			.setFooter(`ID: ${id}`);
+
+		msg.channel.send({embeds: [em]});
 	}
 }
 
@@ -273,6 +291,118 @@ client.on('messageCreate', async (msg) => {
 			break;
 		case prefix + 'help':
 			msg.channel.send(helpMsg.replaceAll('?', prefix));
+			break;
+		case prefix + 'quiz':
+			if(tokens.length != 2) {
+				msg.channel.send(':x: You have to specify a quiz id!');
+				break;
+			}
+
+			id = Math.floor(Number(tokens[1]));
+			if(isNaN(id)) {
+				msg.channel.send(':x: Id must be an positive integer!');
+				break;
+			}
+
+			var quiz = (await db.collection('quiz').doc(tokens[1]).get());
+			if(!quiz.exists) {
+				msg.channel.send('Not found :thinking:');
+				break;
+			}
+
+			var data = quiz.data();
+			sendQuiz(msg, data, id);
+
+			break;
+
+		case prefix + 'add-quiz':
+			newQuiz = {};
+			newQuiz.type = 0;
+
+			const filter = m => {return m.author.id == msg.author.id};
+
+			msg.channel.send(`Question's points (enter \`cancel\` to cancel):`);
+			msg.channel.awaitMessages({
+				filter,
+				max: 1,
+				time: 20000,
+				errors: ['time']
+			})
+			.then((collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var score = Math.floor(Number(info));
+				if(isNaN(score)) {
+					collected.first().reply(':x: Value error!');
+					throw {name: "ValueError", message: "Value error!"};
+				}
+				newQuiz.score = score;
+
+				msg.channel.send(`Question's statement (enter \`cancel\` to cancel):`);
+				return msg.channel.awaitMessages({
+					filter,
+					max: 1,
+					time: 20000,
+					errors: ['time']
+				});
+			})
+			.then((collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var statement = info;
+				newQuiz.question = statement;
+
+				msg.channel.send(`Question's answers (use \`|\` to seperate answers, enter \`cancel\` to cancel):`);
+				return msg.channel.awaitMessages({
+					filter,
+					max: 1,
+					time: 20000,
+					errors: ['time']
+				});
+			})
+			.then((collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var answers = info.split('|');
+				newQuiz.answers = answers;
+
+				msg.channel.send(`Question's tags (use \`|\` to seperate tags, enter \`cancel\` to cancel):`);
+				return msg.channel.awaitMessages({
+					filter,
+					max: 1,
+					time: 20000,
+					errors: ['time']
+				});
+			})
+			.then(async (collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var tag = info.split('|');
+				newQuiz.tag = tag;
+
+				var count = (await db.collection('misc').doc('quiz-info').get('count')).data().count;
+				db.collection('misc').doc('quiz-info').set({count: count + 1});
+				db.collection('quiz').doc(String(count + 1)).set(newQuiz).then(() => {
+					msg.channel.send(':white_check_mark: Add successfully!');
+				});
+			})
+			.catch((e) => {
+				if(e.name != 'OperationCancel' && e.name != 'ValueError')
+					msg.channel.send(':x: No answer provided!');
+			});
+
 			break;
 	}
 });
