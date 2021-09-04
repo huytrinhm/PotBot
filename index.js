350cd71918ddff13e2391073a4f435817968b5cd
Huy Trịnh <huytrinhm@gmail.com>
2021-09-04 12:04:05 +0700

    Fix add-quiz error and add edit-quiz command

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const cmds = [
 	'info',
 	'help',
 	'quiz',
-	'random-quiz',
+	'edit-quiz',
 	'endless-quiz',
 	'add-quiz'
 ];
@@ -125,17 +125,22 @@ async function setCommand(changeId, msg, opt) {
 }
 
 function sendQuiz(msg, data, id) {
-	if(data.type == 0) {
-		var em = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle(`${data.score} points question`)
-			.setDescription(data.question)
-			.addField('Answers', `||${data.answers.join('|')}||`)
-			.addField('Tags', data.tag.join(', '))
-			.setFooter(`ID: ${id}`);
+	try {
+		if(data.type == 0) {
+			var em = new MessageEmbed()
+				.setColor('#0099ff')
+				.setTitle(`${data.score} points question`)
+				.setDescription(data.question)
+				.addField('Answers', `||${data.answers.join('|')}||`)
+				.addField('Tags', data.tag.join(', '))
+				.setFooter(`ID: ${id}`);
 
-		msg.channel.send({embeds: [em]});
+			msg.channel.send({embeds: [em]});
+		}
+	} catch(e) {
+		msg.channel.send(':x: Unexpected error!');
 	}
+
 }
 
 const client = new Client({ intents: [
@@ -156,7 +161,7 @@ client.on('messageCreate', async (msg) => {
 		return;
 
 	
-	tokens = msg.content.split(' ');
+	var tokens = msg.content.split(' ');
 	if(!cmds.includes(tokens[0].slice(1)))
 		return;
 
@@ -305,7 +310,7 @@ client.on('messageCreate', async (msg) => {
 				break;
 			}
 
-			id = Math.floor(Number(tokens[1]));
+			var id = Math.floor(Number(tokens[1]));
 			if(isNaN(id)) {
 				msg.channel.send(':x: Id must be an positive integer!');
 				break;
@@ -321,12 +326,14 @@ client.on('messageCreate', async (msg) => {
 			sendQuiz(msg, data, id);
 
 			break;
-
 		case prefix + 'add-quiz':
-			newQuiz = {};
+			var newQuiz = {};
 			newQuiz.type = 0;
 
-			const filter = m => {return m.author.id == msg.author.id};
+
+			var count = (await db.collection('misc').doc('quiz-info').get('count')).data().count;
+			db.collection('misc').doc('quiz-info').set({count: count + 1});
+			var filter = m => {return m.author.id == msg.author.id};
 
 			msg.channel.send(`Question's points (enter \`cancel\` to cancel):`);
 			msg.channel.awaitMessages({
@@ -399,10 +406,118 @@ client.on('messageCreate', async (msg) => {
 				var tag = info.split('|');
 				newQuiz.tag = tag;
 
-				var count = (await db.collection('misc').doc('quiz-info').get('count')).data().count;
-				db.collection('misc').doc('quiz-info').set({count: count + 1});
 				db.collection('quiz').doc(String(count + 1)).set(newQuiz).then(() => {
 					msg.channel.send(':white_check_mark: Add successfully!');
+				});
+			})
+			.catch((e) => {
+				db.collection('misc').doc('quiz-info').set({count: count});
+				if(e.name != 'OperationCancel' && e.name != 'ValueError')
+					msg.channel.send(':x: No answer provided!');
+			});
+
+			break;
+		case prefix + 'edit-quiz':
+			if(tokens.length != 2) {
+				msg.channel.send(':x: You have to specify a quiz id!');
+				break;
+			}
+
+			var id = Math.floor(Number(tokens[1]));
+			if(isNaN(id)) {
+				msg.channel.send(':x: Id must be an positive integer!');
+				break;
+			}
+
+			var quiz = (await db.collection('quiz').doc(tokens[1]).get());
+			if(!quiz.exists) {
+				msg.channel.send('Not found :thinking:');
+				break;
+			}
+
+			var data = quiz.data();
+			msg.channel.send('Old quiz:');
+			sendQuiz(msg, data, id);
+			msg.channel.send('Please set new quiz:');
+
+			var newQuiz = {};
+			newQuiz.type = 0;
+
+			var filter = m => {return m.author.id == msg.author.id};
+
+			msg.channel.send(`Question's points (enter \`cancel\` to cancel):`);
+			msg.channel.awaitMessages({
+				filter,
+				max: 1,
+				time: 20000,
+				errors: ['time']
+			})
+			.then((collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var score = Math.floor(Number(info));
+				if(isNaN(score)) {
+					collected.first().reply(':x: Value error!');
+					throw {name: "ValueError", message: "Value error!"};
+				}
+				newQuiz.score = score;
+
+				msg.channel.send(`Question's statement (enter \`cancel\` to cancel):`);
+				return msg.channel.awaitMessages({
+					filter,
+					max: 1,
+					time: 20000,
+					errors: ['time']
+				});
+			})
+			.then((collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var statement = info;
+				newQuiz.question = statement;
+
+				msg.channel.send(`Question's answers (use \`|\` to seperate answers, enter \`cancel\` to cancel):`);
+				return msg.channel.awaitMessages({
+					filter,
+					max: 1,
+					time: 20000,
+					errors: ['time']
+				});
+			})
+			.then((collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var answers = info.split('|');
+				newQuiz.answers = answers;
+
+				msg.channel.send(`Question's tags (use \`|\` to seperate tags, enter \`cancel\` to cancel):`);
+				return msg.channel.awaitMessages({
+					filter,
+					max: 1,
+					time: 20000,
+					errors: ['time']
+				});
+			})
+			.then(async (collected) => {
+				var info = collected.first().content;
+				if(info == 'cancel') {
+					collected.first().reply('Operation was cancelled!');
+					throw {name: "OperationCancel", message: "Operation was cancelled!"};
+				}
+				var tag = info.split('|');
+				newQuiz.tag = tag;
+
+				db.collection('quiz').doc(String(id)).set(newQuiz).then(() => {
+					msg.channel.send(':white_check_mark: Edit successfully!');
 				});
 			})
 			.catch((e) => {
